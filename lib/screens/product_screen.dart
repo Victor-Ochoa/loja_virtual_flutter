ddaf21203d318776eb6e07bfca044d58fd43e714
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-07-12 09:56:24 -0300

    titulo e preço do produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
@@ -10,14 +11,55 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-  
   final ProductData product;
   _ProductScreenState(this.product);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              dotVerticalPadding: 4,
+              dotSize: 4,
+              dotSpacing: 15,
+              dotColor: primaryColor,
+              dotIncreasedColor: primaryColor,
+              autoplay: false,
+              dotBgColor: Colors.transparent,
+              images: product.images.map((url) => Image.network(url)).toList(),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }

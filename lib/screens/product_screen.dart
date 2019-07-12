f46af7ecd644f499f0873e61fa80f2a6b0e6d23a
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-07-12 15:02:15 -0300

    tela de produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -12,7 +12,10 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+
   _ProductScreenState(this.product);
+
+  String size = "";
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +57,74 @@ class _ProductScreenState extends State<ProductScreen> {
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
                       color: primaryColor),
+                ),
+                Divider(
+                  color: Colors.transparent,
+                  height: 16,
+                ),
+                Text(
+                  "Tamanho:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 0.5),
+                    children: product.sizes
+                        .map((size) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  this.size = size;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(4)),
+                                  border: Border.all(
+                                      color: this.size == size
+                                          ? primaryColor
+                                          : Colors.grey[500],
+                                      width: 3),
+                                ),
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(size),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                ),
+                Divider(
+                  height: 16,
+                  color: Colors.transparent,
+                ),
+                SizedBox(
+                    height: 44,
+                    child: RaisedButton(
+                      onPressed: this.size.isEmpty ? null : () {},
+                      child: Text(
+                        "Adicionar ao Carrinho",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      color: primaryColor,
+                    )),
+                Divider(
+                  color: Colors.transparent,
+                  height: 16,
+                ),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16),
                 )
               ],
             ),

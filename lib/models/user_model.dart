cad81d56d288a3dfd8a64570b5c57a370e1f3526
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-10-15 13:49:52 -0300

    função de esqueci senha

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -76,7 +76,9 @@ class UserModel extends Model {
     _processing(false);
   }
 
-  void recoverPass() {}
+  void recoverPass(String email) {
+    _auth.sendPasswordResetEmail(email: email);
+  }
 
   bool isLoggedIn() {
     return firebaseUser != null;

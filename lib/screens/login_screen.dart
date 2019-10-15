970579cb02a31209b8923a547af1adb800036897
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-10-15 01:40:38 -0300

    User model e função de cadastro

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
@@ -7,81 +9,91 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Entrar"),
-        centerTitle: true,
-        actions: <Widget>[
-          FlatButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => SignUpScreen()));
-            },
-            child: Text(
-              "Criar Conta",
-              style: TextStyle(fontSize: 15, color: Colors.white),
-            ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(hintText: "E-mail"),
-                keyboardType: TextInputType.emailAddress,
-                validator: (text) {
-                  if (text == null || text.isEmpty || !text.contains("@"))
-                    return "E-mail inválido!";
-                },
+        appBar: AppBar(
+          title: Text("Entrar"),
+          centerTitle: true,
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => SignUpScreen()));
+              },
+              child: Text(
+                "Criar Conta",
+                style: TextStyle(fontSize: 15, color: Colors.white),
               ),
-              Divider(
-                height: 16,
-                color: Colors.transparent,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Senha"),
-                obscureText: true,
-                validator: (text) {
-                  if (text == null || text.isEmpty) return "Senha invalida!";
-                  if (text.length < 6) return "Senha menos que 6 caracteres!";
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Esqueci minha senha",
-                    textAlign: TextAlign.right,
-                  ),
-                  padding: EdgeInsets.zero,
-                ),
-              ),
-              Divider(
-                color: Colors.transparent,
-                height: 16,
-              ),
-              SizedBox(
-                height: 44,
-                child: RaisedButton(
-                  onPressed: () {
-                    if (!_formKey.currentState.validate()) return;
-                  },
-                  child: Text(
-                    "Entrar",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  color: Theme.of(context).primaryColor,
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
-      ),
-    );
+        body: ScopedModelDescendant<UserModel>(
+          builder: (contex, child, model) {
+            if (model.isLoading)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "E-mail"),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (text) {
+                        if (text == null || text.isEmpty || !text.contains("@"))
+                          return "E-mail inválido!";
+                      },
+                    ),
+                    Divider(
+                      height: 16,
+                      color: Colors.transparent,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: "Senha"),
+                      obscureText: true,
+                      validator: (text) {
+                        if (text == null || text.isEmpty)
+                          return "Senha invalida!";
+                        if (text.length < 6)
+                          return "Senha menos que 6 caracteres!";
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Esqueci minha senha",
+                          textAlign: TextAlign.right,
+                        ),
+                        padding: EdgeInsets.zero,
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.transparent,
+                      height: 16,
+                    ),
+                    SizedBox(
+                      height: 44,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (!_formKey.currentState.validate()) {}
+                          model.signIn();
+                        },
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,83 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _addressController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Criar Conta"),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(hintText: "Nome Completo"),
-                validator: (text) {
-                  if (text == null || text.isEmpty) return "Nome inválido!";
-                },
-              ),
-              Divider(
-                height: 16,
-                color: Colors.transparent,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "E-mail"),
-                keyboardType: TextInputType.emailAddress,
-                validator: (text) {
-                  if (text == null || text.isEmpty || !text.contains("@"))
-                    return "E-mail inválido!";
-                },
-              ),
-              Divider(
-                height: 16,
-                color: Colors.transparent,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Senha"),
-                obscureText: true,
-                validator: (text) {
-                  if (text == null || text.isEmpty) return "Senha invalida!";
-                  if (text.length < 6) return "Senha menos que 6 caracteres!";
-                },
-              ),
-              Divider(
-                color: Colors.transparent,
-                height: 16,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Endereço"),
-                validator: (text) {
-                  if (text == null || text.isEmpty) return "Endereço invalida!";
-                },
-              ),
-              Divider(
-                color: Colors.transparent,
-                height: 16,
-              ),
-              SizedBox(
-                height: 44,
-                child: RaisedButton(
-                  onPressed: () {
-                    if (!_formKey.currentState.validate()) return;
-                  },
-                  child: Text(
-                    "Criar Conta",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  color: Theme.of(context).primaryColor,
-                ),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("Criar Conta"),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: ScopedModelDescendant<UserModel>(builder: (contex, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return SingleChildScrollView(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(hintText: "Nome Completo"),
+                    validator: (text) {
+                      if (text == null || text.isEmpty) return "Nome inválido!";
+                    },
+                  ),
+                  Divider(
+                    height: 16,
+                    color: Colors.transparent,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(hintText: "E-mail"),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (text) {
+                      if (text == null || text.isEmpty || !text.contains("@"))
+                        return "E-mail inválido!";
+                    },
+                  ),
+                  Divider(
+                    height: 16,
+                    color: Colors.transparent,
+                  ),
+                  TextFormField(
+                    controller: _passController,
+                    decoration: InputDecoration(hintText: "Senha"),
+                    obscureText: true,
+                    validator: (text) {
+                      if (text == null || text.isEmpty)
+                        return "Senha invalida!";
+                      if (text.length < 6)
+                        return "Senha menos que 6 caracteres!";
+                    },
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(hintText: "Endereço"),
+                    validator: (text) {
+                      if (text == null || text.isEmpty)
+                        return "Endereço invalida!";
+                    },
+                  ),
+                  Divider(
+                    color: Colors.transparent,
+                    height: 16,
+                  ),
+                  SizedBox(
+                    height: 44,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (!_formKey.currentState.validate()) {
+                          model.signUp(
+                              userData: {
+                                "name": _nameController.text,
+                                "email": _emailController.text,
+                                "address": _addressController.text
+                              },
+                              pass: _passController.text,
+                              onSuccess: _onSuccess,
+                              onFail: _onFail);
+                          return;
+                        }
+                      },
+                      child: Text(
+                        "Criar Conta",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }));
   }
+
+  void _onSuccess() {}
+
+  void _onFail() {}
 }

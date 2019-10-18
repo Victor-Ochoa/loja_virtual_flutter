69c7dae2cbae22bcece1bd2c31f550823888cbc1
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-10-18 00:38:03 -0300

    frete

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:loja_virtual/widget/discount_cart.dart';
+import 'package:loja_virtual/widget/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -89,7 +90,8 @@ class CartScreen extends StatelessWidget {
                       .map((product) => CartTile(product))
                       .toList(),
                 ),
-                DiscountCart()
+                DiscountCart(),
+                ShipCard()
               ],
             );
         },

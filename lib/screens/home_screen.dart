c96df5351f05d202618f0d73d95f2dc134e02c93
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-11-12 13:04:58 -0300

    finish app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
 import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widget/cart_button.dart';
 import 'package:loja_virtual/widget/custom_drawer.dart';
@@ -27,7 +28,11 @@ class HomeScreen extends StatelessWidget {
           drawer: CustomDrawer(_pageController),
         body: ProductsTab(),
         ),
-        Container(color: Colors.white,), 
+        Scaffold(
+          appBar: AppBar(title: Text("Lojas"), centerTitle: true,),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pageController),
+        ), 
         Scaffold(
           appBar: AppBar(title: Text("Meus Pedidos"), centerTitle: true,),
           body: OrdersTab(),
